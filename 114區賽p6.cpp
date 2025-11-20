b192c6656080bc93f6ad1024526ee8d98fc9ff5d
RainYang731 <RainYang970124@gmail.com>
2025-11-20 13:29:58 +0800

    #

--- 114區賽p6.cpp
+++ 114區賽p6.cpp
@@ -11,15 +11,25 @@ bool compare(vector<int> arr,vector<int> brr){
 }
 
 signed main(){
-    int n,m;cin>>n>>m;
-    int arr[n+1],brr[m+1];
-    for(int i=1;i<=n;i++) cin>>arr[i];
-    for(int i=1;i<=m;i++) cin>>brr[i];
+    string a,b;
+    getline(cin,a);
+    getline(cin,b);
+    vector<int> arr(1,0),brr(1,0);
+    int n,m;
+
+    for(int i=0;i<a.size();i++){
+        if(a[i]>='0' && a[i]<='9') arr.push_back(a[i]-'0');
+    }
+    for(int i=0;i<b.size();i++){
+        if(b[i]>='0' && b[i]<='9') brr.push_back(b[i]-'0');
+    }
+    
+    n = arr.size();m = brr.size();
     vector<vector<int> > dp(m+1),tmp(2);
     
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<n;i++){
         tmp[1].clear();
-        for(int j=1;j<=m;j++){
+        for(int j=1;j<m;j++){
             tmp[(j+1)%2] = dp[j];
             if(abs(arr[i]-brr[i])<=1){
                 tmp[j%2].push_back(arr[i]);
@@ -33,9 +43,9 @@ signed main(){
         }
     }
     
-    for(int i : dp[m]) cout<<i<<' ';
+    for(int i : dp[m-1]) cout<<i<<' ';
 }
 /*
-dp[m-1]
+dp[m-1] 
 tmp     dp[m]
 */
